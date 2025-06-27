da6b1340580210e46ddbc44c37181e37d50f726b
Tiago Hermano <tiagofhermano@gmail.com>
2025-06-27 18:36:39 -0300

    refactor: configure integration between copilot and blink.cmp

--- lua/community.lua
+++ lua/community.lua
@@ -6,6 +6,6 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   -- import/override with your plugins folder
 }
--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -91,4 +91,37 @@ return {
       vim.cmd.colorscheme "night-owl"
     end,
   },
+
+  -- Configure Copilot integration with Blink.cmp
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    opts = {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
+      },
+    },
+  },
+  {
+    "saghen/blink.cmp",
+    optional = true,
+    dependencies = { "fang2hou/blink-copilot" },
+    opts = {
+      sources = {
+        default = { "copilot" },
+        providers = {
+          copilot = {
+            name = "copilot",
+            module = "blink-copilot",
+            score_offset = 100,
+            async = true,
+          },
+        },
+      },
+    },
+  },
 }
